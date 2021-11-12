c3467886797d9b0e429a11021291e05df533737b
DobriniaMerk <Dobrinia.merk@yandex.ru>
2021-11-12 19:57:59 +0300

    Добавьте файлы проекта.

---
+++ BezieCurve/Curve.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML.System;
+
+namespace BezieCurve
+{
+    class Curve
+    {
+        List<Vector2f> points;
+
+        public Curve()
+        {
+            points = new List<Vector2f>();
+        }
+
+        public void AddPoint(Vector2f pos)
+        {
+            points.Add(pos);
+        }
+
+        Vector2f getPoint(float t, List<Vector2f> p)
+        {
+            if(p.Count == 2)
+            {
+                return (p[1] - p[0]) * t;
+            }
+            else
+            {
+                List<Vector2f> ret = new List<Vector2f>();
+                for(int i = 0; i < p.Count - 1; i++)
+                {
+                    List<Vector2f> r = new List<Vector2f>();
+                    r.Add(p[i]);
+                    r.Add(p[i + 1]);
+                    ret.Add(getPoint(t, r));
+                }
+                return getPoint(t, ret);
+            }
+        }
+    }
+}
---
+++ BezieCurve/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using SFML.System;
+using SFML.Window;
+using SFML.Graphics;
+
+namespace BezieCurve
+{
+    class Program
+    {
+        static Game game;
+        static RenderWindow rw;
+        static void Main(string[] args)
+        {
+            VideoMode vm = new VideoMode(800, 600);
+            rw = new RenderWindow(vm, "Bezie");
+            game = new Game();
+
+            rw.Closed += OnClose;
+
+            while (rw.IsOpen)
+            {
+                rw.DispatchEvents();
+                rw.Clear();
+                game.Draw(rw);
+                rw.Display();
+            }
+        }
+
+        static void OnClose(object sender, EventArgs e)
+        {
+            (sender as RenderWindow)?.Close();
+        }
+    }
+}
